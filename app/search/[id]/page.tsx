83eb07e411d982d05c32cf94bb7ea3fc734f341d
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-15 11:29:27 +0100

    Added search page

---
+++ app/search/[id]/page.tsx
@@ -0,0 +1,11 @@
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+function SearchPage({ params: { id } }: Props) {
+    return <div>SearchPage</div>;
+}
+
+export default SearchPage;
